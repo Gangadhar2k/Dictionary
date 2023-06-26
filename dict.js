429913704a3c3373491f07e8031d931f2e162e23
Gangadhar S <115092868+Gangadhar2k@users.noreply.github.com>
2023-06-26 15:18:05 +0530

    Update dict.js

--- dict.js
+++ dict.js
@@ -3,9 +3,11 @@ class Dictionary {
     const response = await fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${user}`
     );
+    const resData = await response.json()
+    const myTimeout= setTimeout(resData, 5000);
+    return myTimeout;
+   
 
-    setTimeout(const resData = await response.json(),2000);
-
-    return resData;
+  
   }
 }
